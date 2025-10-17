02149a62e438c0396462b459b651b547841f5c2a
Preston Mendonca <preston.mendonca@axionet.in>
2025-10-18 00:37:41 +0530

    Changes in Package

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,6 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 
-if (process.env.NODE_ENV === "development") {
-  require("@vue/devtools-kit");
-}
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
