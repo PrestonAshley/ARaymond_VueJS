f738f3335f30ce14d8288004fa74ab196b2aa3e0
Preston Mendonca <preston.mendonca@axionet.in>
2025-10-18 10:25:21 +0530

    Fix: Conditionally import vite_devtools

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,7 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-// const isDev = process.env.NODE_ENV === "development";
-//
-
-//
-
-import vueDevTools from "vite-plugin-vue-devtools";
+const isDev = process.env.NODE_ENV === "development";
 
 // https://vite.dev/config/
 export default defineConfig(async () => {
