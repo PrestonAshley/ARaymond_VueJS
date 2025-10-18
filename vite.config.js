b55968a403b6e3504d6065f23e872e94ed47dbed
Preston Mendonca <preston.mendonca@axionet.in>
2025-10-18 10:12:22 +0530

    Fix Build: Local Build Successfully

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,15 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-const isDev = process.env.NODE_ENV === "development";
-const plugins = [vue()];
+// const isDev = process.env.NODE_ENV === "development";
+// const plugins = [vue()];
 
-if (isDev) {
-  const vueDevTools = require("vite-plugin-vue-devtools").default;
-  plugins.push(vueDevTools());
-}
+// if (isDev) {
+//   const vueDevTools = require("vite-plugin-vue-devtools").default;
+//   plugins.push(vueDevTools());
+// }
+
+import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
